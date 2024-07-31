3f23c8d54d25a5e04a6dca54e5a21230463688e9
NatsukiHayashi <su811sh1@gmail.com>
2024-07-31 09:42:10 +0900

    :sparkles: 3-2-2

--- src/chapter3/ForItem.js
+++ src/chapter3/ForItem.js
@@ -1,4 +1,43 @@
 import React from "react";
+import Download from "./Download";
+
+// export function ForItem({ book }) {
+//     let dd;
+//     if (book.download) {
+//         dd = <dd>{book.summary}<Download isbn={book.isbn} /></dd>
+//     } else {
+//         dd = <dd>{book.summary}</dd>
+//     }
+//     return (
+//         <>
+//             <dt>
+//                 <a href={`https;//wings.msn.to/books/${book.isbn}/${book.isbn}.jpeg`}>
+//                     {book.title} ({book.price}円)
+//                 </a>
+//             </dt>
+//             {dd}
+//         </>
+//     );
+// }
+
+// export function ForItem({ book }) {
+//     return (
+//         <>
+//             <dt>
+//                 <a href={`https;//wings.msn.to/books/${book.isbn}/${book.isbn}.jpeg`}>
+//                     {book.title} ({book.price}円)
+//                 </a>
+//             </dt>
+//             {(() => {
+//                 if (book.download) {
+//                     return <dd>{book.summary}<Download isbn={book.isbn} /></dd>
+//                 } else {
+//                     return <dd>{book.summary}</dd>
+//                 }
+//             })()}
+//         </>
+//     );
+// }
 
 export function ForItem({ book }) {
     return (
@@ -8,7 +47,11 @@ export function ForItem({ book }) {
                     {book.title} ({book.price}円)
                 </a>
             </dt>
-            <dd>{book.summary}</dd>
+            <dd>
+                {book.summary}
+                {/*{book.download && <Download isbn={book.isbn} />}*/}
+                {book.download ? <Download isbn={book.isbn} /> : null}
+            </dd>
         </>
     );
 }
