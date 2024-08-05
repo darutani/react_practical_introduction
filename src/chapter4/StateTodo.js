a78966f4bf974dda2bc24efba8958faddc87dd8c
NatsukiHayashi <su811sh1@gmail.com>
2024-08-05 09:25:17 +0900

    :sparkles: 4-2-3

--- src/chapter4/StateTodo.js
+++ src/chapter4/StateTodo.js
@@ -1,9 +1,11 @@
 import {useState} from "react";
+import './StateTodo.css';
 
 let maxId = 0;
 export default function StateTodo() {
     const [title, setTitle] = useState('');
     const [todo, setTodo] = useState([]);
+    const [desc, setDesc] = useState(true);
     const handleChangeTitle = e => {
         setTitle(e.target.value)
     }
@@ -18,15 +20,47 @@ export default function StateTodo() {
             },
         ]);
     };
+    const handleDone = e => {
+        setTodo(todo.map(item => {
+            if (item.id === Number(e.target.dataset.id)) {
+                return {
+                    ...item,
+                    isDone: true,
+                }
+            } else {
+                return item;
+            }
+        }));
+    };
+    const handleRemove = e => {
+        setTodo(todo.filter(item => item.id !== Number(e.target.dataset.id)));
+    };
+    const handleSort = e => {
+        const sorted = [...todo];
+        sorted.sort((m, n) => {
+            if (desc) {
+                return n.created.getTime() - m.created.getTime();
+            } else {
+                return m.created.getTime() - n.created.getTime();
+            }
+        })
+        setDesc(d => ! d);
+        setTodo(sorted);
+    }
     return (
         <div>
             <label htmlFor="title">やること：</label>
             <input id="titile" name="title" value={title} onChange={handleChangeTitle} />
             <button type="button" onClick={handleClick}>追加</button>
+            <button type="button" onClick={handleSort}>ソート{desc ? '↑' : '↓'}</button>
             <hr />
             <ul>
                 {todo.map(item => (
-                    <li key={item.id}>{item.title}</li>
+                    <li key={item.id} className={item.isDone ? 'done' : ''}>
+                        {item.title}
+                        <button type="button" onClick={handleDone} data-id={item.id}>済</button>
+                        <button type="button" onClick={handleRemove} data-id={item.id}>削除</button>
+                    </li>
                 ))}
             </ul>
         </div>
