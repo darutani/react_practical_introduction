b78eadd25574a1c989e90e04c341fab823fe17c6
NatsukiHayashi <su811sh1@gmail.com>
2024-08-06 09:15:33 +0900

    :sparkles: 4-3-3

--- src/chapter4/FormBasic.js
+++ src/chapter4/FormBasic.js
@@ -8,10 +8,20 @@ export default function FormBasic() {
         memo: '',
     };
 
-    const { register, handleSubmit, formState: { errors } } = useForm({
+    const { register,
+        handleSubmit,
+        formState: { errors, isDirty, isValid, isSubmitting }
+    } = useForm({
         defaultValues
     });
-    const onsubmit = data => console.log(data);
+    const onsubmit = data => {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                resolve();
+                console.log(data);
+            }, 4000);
+        });
+    };
     const onerror = err => console.log(err);
 
     return (
@@ -85,7 +95,8 @@ export default function FormBasic() {
                 <div>{errors.memo?.message}</div>
             </div>
             <div>
-                <button type="submit">送信</button>
+                <button type="submit" disabled={! isDirty || ! isValid || isSubmitting}>送信</button>
+                {isSubmitting && <div>...送信中...</div>}
             </div>
         </form>
     );
