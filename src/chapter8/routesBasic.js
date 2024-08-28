72c67e8d115b67337ff65a1994a2e2729f9d1f86
NatsukiHayashi <su811sh1@gmail.com>
2024-08-28 09:21:38 +0900

    :sparkles: 8-1-3

--- src/chapter8/routesBasic.js
+++ src/chapter8/routesBasic.js
@@ -1,13 +1,17 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
 import {element} from "prop-types";
 import TopPage from "./TopPage";
 import AboutPage from "./AboutPage";
 import ArticlePage from "./ArticlePage";
 
-const routesBasic = createBrowserRouter([
-    { path: '/', element: <TopPage /> },
-    { path: '/article', element: <ArticlePage /> },
-    { path: '/about', element: <AboutPage /> },
-]);
+const routesBasic = createBrowserRouter(
+    createRoutesFromElements(
+        <>
+            <Route path="/" element={<TopPage />} />
+            <Route path="/article" element={<ArticlePage />} />
+            <Route path="/about" element={<AboutPage />} />
+        </>
+    )
+);
 
 export default routesBasic;
