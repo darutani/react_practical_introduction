ff228e877bd928e9b464b6101844905396cabb85
NatsukiHayashi <su811sh1@gmail.com>
2024-08-30 08:46:34 +0900

    :sparkles: 8-2-3

--- src/chapter8/RouterApp.js
+++ src/chapter8/RouterApp.js
@@ -4,9 +4,10 @@ export default function RouterApp() {
     return (
         <>
             <ul>
-                <li><Link to={"/"}>トップ</Link></li>
+                <li><Link to={"/"} reloadDocument>トップ</Link></li>
                 <li><Link to={"/article"}>公開記事</Link></li>
-                <li><Link to={"/about"}>このサイトについて</Link></li>
+                <li><Link to={"/about"} replace>このサイトについて</Link></li>
+                <li><Link to={"/article/info"}>Infoページ</Link></li>
             </ul>
             <hr />
             <Outlet />
