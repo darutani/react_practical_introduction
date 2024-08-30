c40d30fc9b1c1ed9aa95cf9f2abeedd9faec1a10
NatsukiHayashi <su811sh1@gmail.com>
2024-08-30 22:33:19 +0900

    :sparkles: 8-3-4

--- src/chapter8/routesParam.js
+++ src/chapter8/routesParam.js
@@ -5,6 +5,7 @@ import RouterParams from "./RouterParams";
 import SearchPage from "./SearchPage";
 import NotFoundPage from "./NotFoundPage";
 import BigQueryPage from "./BigQueryPage";
+import BookStatePage from "./BookStatePage";
 
 const routesParam = createBrowserRouter(
     createRoutesFromElements(
@@ -12,6 +13,7 @@ const routesParam = createBrowserRouter(
             <Route path="/" element={<TopPage />} />
             <Route path="/book/:isbn?" element={<BookPage />} />
             <Route path="/bookQuery" element={<BigQueryPage />} />
+            <Route path="/bookState" element={<BookStatePage />} />
             <Route path="/search/*" element={<SearchPage />} />
             <Route path="*" element={<NotFoundPage />} />
         </Route>
