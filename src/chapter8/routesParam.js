52fd37348a2e96ff3ca536aaa53cfe88f64f1eee
NatsukiHayashi <su811sh1@gmail.com>
2024-09-14 23:40:54 +0900

    :sparkles: レスポンスを自分で作成する

--- src/chapter8/routesParam.js
+++ src/chapter8/routesParam.js
@@ -1,4 +1,4 @@
-import {createBrowserRouter, createRoutesFromElements, Route} from "react-router-dom";
+import {createBrowserRouter, createRoutesFromElements, json, Route} from "react-router-dom";
 import TopPage from "./TopPage";
 import BookPage from "./BookPage";
 import RouterParams from "./RouterParams";
@@ -9,6 +9,17 @@ import BookStatePage from "./BookStatePage";
 import InvalidParamsPage from "./InvalidParamsPage";
 import WeatherPage from "./WeatherPage";
 
+const fetchWeather = async ({ params }) => {
+    const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=ja&appid=50eee5976b25f93049deb7fd5d14e6bd`)
+    if (res.ok) { return res; }
+    return json({
+        "weather": [
+            {"id":803, "main": "Unknown", "description": "不明", "icon": "50d"}
+        ],
+        "name": "不明"
+    });
+}
+
 const routesParam = createBrowserRouter(
     createRoutesFromElements(
         <Route element={<RouterParams />} errorElement={<InvalidParamsPage />}>
@@ -18,9 +29,7 @@ const routesParam = createBrowserRouter(
             <Route path="/bookState" element={<BookStatePage />} />
             <Route path="/search/*" element={<SearchPage />} />
             <Route path="/weather/:city" element={<WeatherPage />}
-                loader={({ params}) =>
-                    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=ja&appid=50eee5976b25f93049deb7fd5d14e6bd`)
-                }
+                loader={fetchWeather}
             />
             <Route path="*" element={<NotFoundPage />} />
         </Route>
