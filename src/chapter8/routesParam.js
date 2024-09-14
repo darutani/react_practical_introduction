d7f4cbfdc9e10ac37cc839c3f1759aa9b5f897e6
NatsukiHayashi <su811sh1@gmail.com>
2024-09-14 23:54:52 +0900

    :sparkles: json関数（Responseオブジェクト）をスローする

--- src/chapter8/routesParam.js
+++ src/chapter8/routesParam.js
@@ -8,21 +8,31 @@ import BigQueryPage from "./BigQueryPage";
 import BookStatePage from "./BookStatePage";
 import InvalidParamsPage from "./InvalidParamsPage";
 import WeatherPage from "./WeatherPage";
+import CommonErrorPage from "./CommonErrorPage";
 
 const fetchWeather = async ({ params }) => {
     const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${params.city}&lang=ja&appid=50eee5976b25f93049deb7fd5d14e6bd`)
     if (res.ok) { return res; }
-    return json({
-        "weather": [
-            {"id":803, "main": "Unknown", "description": "不明", "icon": "50d"}
-        ],
-        "name": "不明"
-    });
+    // return json({
+    //     "weather": [
+    //         {"id":803, "main": "Unknown", "description": "不明", "icon": "50d"}
+    //     ],
+    //     "name": "不明"
+    // });
+
+    switch (res.status) {
+        case 404:
+            throw json({ message: 'city is invalid!' }, { status: 404 });
+        case 401:
+            throw json({ message: 'api key is invalid!' }, { status: 401 });
+        default:
+            throw json({ message: 'api server is in throuble...' }, { status: 501 });
+    }
 }
 
 const routesParam = createBrowserRouter(
     createRoutesFromElements(
-        <Route element={<RouterParams />} errorElement={<InvalidParamsPage />}>
+        <Route element={<RouterParams />} errorElement={<CommonErrorPage />}>
             <Route path="/" element={<TopPage />} />
             <Route path="/book/:isbn?" element={<BookPage />} />
             <Route path="/bookQuery" element={<BigQueryPage />} />
