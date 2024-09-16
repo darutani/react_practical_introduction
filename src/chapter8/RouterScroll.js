e5a0bae30f357ebc566e7cbf32832f11673c0929
NatsukiHayashi <su811sh1@gmail.com>
2024-09-16 12:05:29 +0900

    :sparkles: 8-5-2 スクロールの復元のルールを変更する

--- src/chapter8/RouterScroll.js
+++ src/chapter8/RouterScroll.js
@@ -3,7 +3,7 @@ import {Outlet, ScrollRestoration} from "react-router-dom";
 export default function RouterScroll() {
     return (
         <>
-            <ScrollRestoration />
+            <ScrollRestoration getKey={(location, matches) => location.pathname}  />
             <Outlet />
         </>
     );
